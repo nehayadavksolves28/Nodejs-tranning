1f1034cd7154027c2a60fe89cf9f59b974c2a09f
Neha Yadav <neha.yadav@ksolves.com>
2025-05-21 21:57:04 +0530

    js to ts code added

---
+++ typeScriptPractice/index.js
@@ -0,0 +1,10 @@
+// function add(a, b) {
+//     return a + b;
+//   }
+//   const result = add(5, 10);
+//   console.log("Result:", result);
+function add(a, b) {
+    return a + b;
+}
+var result = add(5, 10);
+console.log("Result:", result);
---
+++ typeScriptPractice/index.ts
@@ -0,0 +1,16 @@
+// function add(a, b) {
+//     return a + b;
+//   }
+  
+//   const result = add(5, 10);
+//   console.log("Result:", result);
+  
+
+
+function add(a: number, b: number): number {
+    return a + b;
+  }
+  
+  const result: number = add(5, 10);
+  console.log("Result:", result);
+  
